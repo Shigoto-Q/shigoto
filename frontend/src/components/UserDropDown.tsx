ec6ef01f629ac35f9a2265eaa45b9de5324cc720
Simeon <aleksov_s@outlook.com>
2021-06-17 20:14:45 +0200

    Adds theme toggle Dark theme

--- frontend/src/components/UserDropDown.tsx
+++ frontend/src/components/UserDropDown.tsx
@@ -6,72 +6,80 @@ import { logout } from "../redux/actions/auth"
 import { connect } from "react-redux"
 
 function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(' ')
 }
 
 type DropDownProps = {
-  logout: any
+    logout: any
+    user: string
 }
-const UserDropDown = ({ logout }: DropDownProps) => {
-  return (
-    <Menu as="div" className="relative inline-block text-left">
-      {({ open }) => (
-        <>
-          <div>
-            <Menu.Button className="inline-flex justify-center w-full shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-              <User className="" aria-hidden="true" />
-            </Menu.Button>
-          </div>
+const UserDropDown = ({ logout, user}: DropDownProps) => {
+    return (
+        <Menu as="div" className="relative inline-flex dark:text-gray-300">
+            {({ open }) => (
+                <>
+                    <div>
+                        <Menu.Button className="inline-flex justify-center items-center group">
+                            <User className="" aria-hidden="true" />
+                            <div className="flex items-center truncate">
+                                <span className="truncate ml-2 text-sm font-medium group-hover:text-gray-800">{user}</span>
+                                <svg className="w-3 h-3 flex-shrink-0 ml-1 fill-current text-gray-400" viewBox="0 0 12 12">
+                                    <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                                </svg>
+                            </div>
 
-          <Transition
-            show={open}
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items
-              static
-              className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-            >
-              <div className="py-1">
-                <Menu.Item>
-                  {({ active }) => (
-                    <Link
-                      to="/dashboard/profile-settings"
-                      className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm'
-                      )}
+                        </Menu.Button>
+                    </div>
+
+                    <Transition
+                        show={open}
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
                     >
-                      Account settings
-                    </Link>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <Link
-                      to="/"
-                      className={classNames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block w-full text-left px-4 py-2 text-sm'
-                      )}
-                      onClick={logout}
-                    >
-                      Sign out
-                    </Link>
-                  )}
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Transition>
-        </>
-      )}
-    </Menu>
-  )
+                        <Menu.Items
+                            static
+                            className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        >
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <Link
+                                            to="/dashboard/profile-settings"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Account settings
+                                        </Link>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <Link
+                                            to="/"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block w-full text-left px-4 py-2 text-sm'
+                                            )}
+                                            onClick={logout}
+                                        >
+                                            Sign out
+                                        </Link>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </>
+            )}
+        </Menu>
+    )
 }
 
 
