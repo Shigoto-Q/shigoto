2ad08bc13b4ee39a2185df6491b9445e8be0326e
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2021-07-05 22:59:00 +0200

    task-create started

--- frontend/src/components/generic/DropdownMenu.tsx
+++ frontend/src/components/generic/DropdownMenu.tsx
@@ -3,27 +3,31 @@ import {Calendar, Check, Icon, Menu} from "react-feather";
 import React, {Fragment} from "react";
 
 type DropdownMenuProps = {
-    selected: { id:number, name:string, value: string },
-    setSelected:  React.Dispatch<React.SetStateAction<{id: number, name:string, value: string}>>,
-    options: Array<{ id:number,name:string, value:string }>
+    selected: { id:number, name: any, value: string },
+    setSelected:  React.Dispatch<React.SetStateAction<{id: number, name:any, value: string}>>,
+    options: Array<{ id:number,name:any, value:string }>
+    label?: string
     children?: any
 }
+
+
 
 function classNames(...classes: any) {
     return classes.filter(Boolean).join(' ')
 }
 
 const DropdownMenu = (props:DropdownMenuProps) => {
+
     return (
         <>
             <Listbox value={props.selected} onChange={props.setSelected}>
                 {({ open }) => (
                     <>
-                        <Listbox.Label className="block text-sm font-medium text-gray-700">Select your task type: </Listbox.Label>
+                        <Listbox.Label className="block text-sm font-medium text-gray-700">{props.label} </Listbox.Label>
                         <div className="mt-1 relative">
                             <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"><span className="flex items-center">
                                 {props.children}
-                                <span className="ml-3 block">{props.selected.name}</span>
+                                <span className="ml-3 block">{props.selected.name? props.selected.name : props.selected.value}</span>
                             </span>
                                 <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"><Menu className="h-5 w-5 text-gray-400" aria-hidden="true" /></span>
                             </Listbox.Button>
@@ -36,7 +40,7 @@ const DropdownMenu = (props:DropdownMenuProps) => {
                             >
                                 <Listbox.Options
                                     static
-                                    className="absolute mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+                                    className="absolute mt-1 z-10 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
                                 >
                                     {props.options.map((option) => (
                                         <Listbox.Option
@@ -54,7 +58,7 @@ const DropdownMenu = (props:DropdownMenuProps) => {
                                                     <div className="flex items-center">
                                                         <span
                                                             className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
-                                                        >{option.name}
+                                                        >{option.name? option.name : option.value}
                                                         </span>
                                                     </div>
                                                     {selected ? (
