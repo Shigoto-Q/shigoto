d5504031a3c858717af7994ccc98492ac9ce3387
SimeonAleksov <aleksov_s@outlook.com>
2021-09-14 20:05:06 +0200

    clean up

--- frontend/src/routes/routes/index.tsx
+++
@@ -1,5 +0,0 @@
-// TODO Create routes with given layout
-// ex: {uri: /example, component: lazy("ExampleComponent"), layout: "UserLayout"}
-// map and create actual routes
-const Routes = {}
-export default Routes
