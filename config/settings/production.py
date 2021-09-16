ef36f8b25c4aeb1fdad7ae812c569cbbb904dff5
b0gdanp3trovic <62192440+b0gdanp3trovic@users.noreply.github.com>
2021-09-16 20:21:49 +0200

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -21,8 +21,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-#ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
-ALLOWED_HOSTS=["194.195.247.179"]
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = ["194.195.247.179"]
 
 
 # DATABASES
@@ -49,27 +49,27 @@ CACHES = {
 # SECURITY
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
-#SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
-#SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+# SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-#SESSION_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-#CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
-#SECURE_HSTS_SECONDS = 60
+# SECURE_HSTS_SECONDS = 60
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
-#SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
 #    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
-#)
+# )
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
-#SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+# SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
 # https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
-#SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
+# SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 #    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
-#)
+# )
 
 # STATIC
 # ------------------------
@@ -109,16 +109,16 @@ ADMIN_URL = "admin/"
 # Anymail
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-#INSTALLED_APPS += ["anymail"]  # noqa F405
+# INSTALLED_APPS += ["anymail"]  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/mailgun/
-#EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
-#ANYMAIL = {
+# EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+# ANYMAIL = {
 #    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
 #    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
 #    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.mailgun.net/v3"),
-#}
+# }
 
 
 # LOGGING
