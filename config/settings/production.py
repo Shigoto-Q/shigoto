a8dd466a368473d331c233dc95a0112704e35637
b0gdanp3trovic <62192440+b0gdanp3trovic@users.noreply.github.com>
2021-09-16 16:59:04 +0200

    temporary change to production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -91,7 +91,7 @@ EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[Shigoto Q]")
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL regex.
-ADMIN_URL = env("DJANGO_ADMIN_URL")
+ADMIN_URL = "/admin"
 
 # Anymail
 # ------------------------------------------------------------------------------
