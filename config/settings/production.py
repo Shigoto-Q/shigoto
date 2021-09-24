1a5caf2b135c29e070ef16d9078e2931dc789a9e
b0gdanp3trovic <62192440+b0gdanp3trovic@users.noreply.github.com>
2021-09-24 08:22:28 +0200

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ DJSTRIPE_FOREIGN_KEY_TO_FIELD = "id"
 
 DEBUG = False
 SECRET_KEY = env("DJANGO_SECRET_KEY")
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["shigoto.live"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["api.shigoto.live"])
 
 # GENERAL
 # ------------------------------------------------------------------------------
