33855a34a6db984cbde6325aaac1da6f5eef3db0
Simeon Aleksov <aleksov_s@outlook.com>
2022-03-05 11:03:05 +0100

    Fix conflict

--- shigoto_q/tasks/enums.py
+++ shigoto_q/tasks/enums.py
@@ -36,26 +36,6 @@ class TaskEnum(enum.Enum):
     KUBERNETES_JOB = task_messages.Task(2, "Kubernetes job", "kubernetes_job")
 
 
-class TaskStatus(enum.Enum):
-    PENDING = (0, "PENDING")
-    RECEIVED = (1, "RECEIVED")
-    STARTED = (2, "STARTED")
-    SUCCESS = (3, "SUCCESS")
-    FAILURE = (4, "FAILURE")
-    REVOKED = (5, "REVOKED")
-    REJECTED = (6, "REJECTED")
-    RETRY = (7, "RETRY")
-    IGNORED = (8, "IGNORED")
-
-    @classmethod
-    def get_value(cls, name):
-        return cls[name]
-
-    @classmethod
-    def get_values(cls):
-        return [status.value for status in cls]
-
-
 class LogEvent(enum.Enum):
     TASK_RESULTS = "task_results"
     TASK_COUNT = "task_count"
